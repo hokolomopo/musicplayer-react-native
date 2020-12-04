9f766cd2267bf058fa487899eff7161ad9b0c913
hokolomopo <adrien.minne@outlook.com>
2020-12-05 00:22:47 +0100

    Add recyclerview instead of flatlist

--- App.js
+++ App.js
@@ -40,7 +40,7 @@ export default class App extends React.Component {
 
   render() {
     // return(
-    //   <HomeScreen></HomeScreen>
+    //   <RecycleTestComponent></RecycleTestComponent>
     // )
     return (
       <ReduxProvider store={Store}>
--- src/components/tabs/SongsTab.js
+++ src/components/tabs/SongsTab.js
@@ -1,22 +1,49 @@
 import React from 'react';
+import { BaseScrollView, DataProvider, LayoutProvider, RecyclerListView } from "recyclerlistview";
 import {Dimensions, FlatList, StyleSheet, Text, View} from 'react-native';
-import { SceneMap, TabView } from 'react-native-tab-view';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 import { connect } from 'react-redux'
-import { cos } from 'react-native-reanimated';
-import { createStackNavigator } from '@react-navigation/stack';
 
 import MainAppbar from '../MainAppbar';
+import RecycleTestComponent from '../TestComponent';
 import SongItem from '../lists/SongItem'
+
+const dataProvider = new DataProvider((r1, r2) => {
+            // return (r1.artist !== r2.artist) && (r1.title !== r2.title);
+            return r1 !== r2;
+});
+
 
 class SongsTab extends React.Component {
 
     constructor(props) {
         super(props)
+        let { width } = Dimensions.get("window");
+
+        //Create the data provider and provide method which takes in two rows of data and return if those two are different or not.
+        //THIS IS VERY IMPORTANT, FORGET PERFORMANCE IF THIS IS MESSED UP
+
+        this._layoutProvider = new LayoutProvider(
+            index => {
+                return 0
+            },
+            (type, dim) => {
+                dim.width = width;
+                dim.height = 70;
+            }
+        );
+
+        this._rowRenderer = this._rowRenderer.bind(this);
+
+        //Since component should always render once data has changed, make data provider part of the state
         this.state = {
-            songs : [],
+            data: this._generateArray(10)
         };
+
+        // this.state = {
+        //     songs : [],
+        // };
     }
+    
     
     _Separator(){
         return(<View style={styles.separator}/>)
@@ -32,18 +59,36 @@ class SongsTab extends React.Component {
         return{length : 70, offset: 70 * index, index}
     }
 
+    _generateArray(n) {
+        let arr = new Array(n);
+        for (let i = 0; i < n; i++) {
+            arr[i] = {title : "me" ,artist : "elle"};
+        }
+        return arr;
+    }
+
+    //Given type and data return the view component
+    _rowRenderer(type, data) {
+        //You can return any view here, CellContainer has no special significance
+            return(<SongItem artist={data.artist} title={data.title}/>)
+
+    }
+
+    _dataProvider = (data) => {
+        return dataProvider.cloneWithRows(data)        
+    }
+
+
     render() {
-        return (
-            <View style={{ flex: 1}}>
-            <FlatList
-                data={this.props.songs}
-                keyExtractor={(item) => item.title.toString() + item.artist.toString()}
-                renderItem={(item) => this._SongItem(item)}
-                ItemSeparatorComponent={this._Separator}
-                // getItemLayout={this._getItemLayout}
-            />     
-            </View>
-        );
+        console.log("Render")
+        // console.log(this.props)
+        let data = this.props.songs != undefined ? this.props.songs : [{title : "no", artist : "One"}]
+        console.log(data[0])
+        return <RecyclerListView 
+                layoutProvider={this._layoutProvider} 
+                dataProvider={this._dataProvider(data)} 
+                rowRenderer={this._rowRenderer} 
+                renderAheadOffset={1000}/>;
     }
 }
 
@@ -53,11 +98,31 @@ const styles = StyleSheet.create({
         width: "100%",
         backgroundColor: "darkgray",
     },
+
+        container: {
+        justifyContent: "space-around",
+        alignItems: "center",
+        flex: 1,
+        backgroundColor: "#00a1f1"
+    },
+    containerGridLeft: {
+        justifyContent: "space-around",
+        alignItems: "center",
+        flex: 1,
+        backgroundColor: "#ffbb00"
+    },
+    containerGridRight: {
+        justifyContent: "space-around",
+        alignItems: "center",
+        flex: 1,
+        backgroundColor: "#7cbb00"
+    }
 });
 
 const mapStateToProps = (state /*, ownProps*/) => {
+    console.log("MapPorpsToState")
   return {
-    songs: state.songs
+    songs: state.songs,
   }
 }
 
